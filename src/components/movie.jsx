735a0e2d5c92351bb0165aefef3b49ff79474814
lkslks4511 <67831940+lkslks4511@users.noreply.github.com>
2023-10-12 22:40:26 +0900

    Add files via upload

---
+++ src/components/movie.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+const Base_URL = "https://image.tmdb.org/t/p/w1280/"
+
+
+export default function Movie({title, poster_path, vote_average}) {
+    
+    return (
+        <div className="movie-container">
+            <img src={Base_URL + poster_path} alt = "poster"/>
+            <div className="movie-info">        
+                <h4>{title}</h4>
+                <span>{vote_average}</span>
+            </div>
+        </div>
+    )
+}
+
