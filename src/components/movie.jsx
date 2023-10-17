13340c3c96f8cf9d32fbf684f88c5af749086230
lkslks4511 <67831940+lkslks4511@users.noreply.github.com>
2023-10-17 16:53:56 +0900

    Add files via upload

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -1,21 +1,22 @@
-    import React from "react"
-    const Base_URL = "https://image.tmdb.org/t/p/w1280/"
+  import React from "react";
+  import * as S from "./Movie.style";
 
+  const Base_URL = "https://image.tmdb.org/t/p/w1280/";
 
-    export default function Movie({title, poster_path, vote_average, overview}) {
-        
-        return (
-            <div className="movie-container">
-                <img src={Base_URL + poster_path} alt = "poster"/>
-                <div className="movie-info">        
-                    <h4>{title}</h4>
-                    <span>{vote_average}</span>
-                </div>
-                <div className="overview">
-                    <h4>{title}</h4>
-                    <span>{overview}</span>
-                </div>
-            </div>
-        )
-    }
+  const Movie = ({ title, poster_path, vote_average, overview }) => {
+    return (
+      <S.MovieContainer>
+        <img src={Base_URL + poster_path} alt="poster" />
+        <S.MovieInfo>
+          <h4>{title}</h4>
+          <span>{vote_average}</span>
+        </S.MovieInfo>
+        <S.Overview>
+          <h4>{title}</h4>
+          <span>{overview}</span>
+        </S.Overview>
+      </S.MovieContainer>
+    );
+  };
 
+  export default Movie;
